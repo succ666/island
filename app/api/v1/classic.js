1bb71843b7fc8ccd8282ce1b0785dc2e785762b6
succ666 <2014649323@qq.com>
2023-11-19 13:59:00 +0800

    取消点赞接口

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -3,7 +3,7 @@ const router = new Router({
     prefix: '/v1/classic'
 })
 const {Flow} = require('../../../models/flow')
-
+const {Favor} = require('../../../models/favor')
 const { ParameterException } = require('../../../core/http-exception')
 const {PositiveIntegerValidator} = require('../../../validator/validator')
 const {Auth} = require('../../../middlewares/auth')
@@ -13,8 +13,10 @@ router.post('/latest',new Auth(8).m, async (ctx, next) => {
     const flow = await Flow.findOne({
         order: [['index', 'DESC']]
     })
+    let likeStatus = await Favor.userLikeIt(flow.art_id, flow.type, ctx.auth.uid)
     let art = await Art.getData(flow.art_id, flow.type)
     art.setDataValue('index',flow.index)
+    art.setDataValue('like_status', likeStatus ? 1: 0)
     ctx.body = art
 })
 
